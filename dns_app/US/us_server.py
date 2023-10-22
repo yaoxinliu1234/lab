b073a9589034988c487f1eb31e80e2c37ee938df
yaoxinliu1234 <147022999+yaoxinliu1234@users.noreply.github.com>
2023-10-22 00:03:10 -0400

    Update us_server.py

--- dns_app/US/us_server.py
+++ dns_app/US/us_server.py
@@ -21,4 +21,4 @@ def fibonacci():
     return jsonify({'fibonacci': fibonacci_result}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='127.9.0.1', port=8080)
